464b4b9918ed72e3785220222bb950b09b04fecb
Toph92 <cdesbois@free.fr>
2023-02-21 01:54:32 +0100

    sdfs

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import 'package:time_graduations/time_graduation.dart';
+import 'package:time_graduations/time_graduations.dart';
 
 void main() {
   runApp(const MyApp());
